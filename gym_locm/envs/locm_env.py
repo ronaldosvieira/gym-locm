be2266ac659ede56ae6570d19969e73f942ca0ac
Ronaldo Vieira <ronaldoesv@gmail.com>
2019-06-04 16:35:16 -0300

    Read available cards from text file

--- gym_locm/envs/locm_env.py
+++ gym_locm/envs/locm_env.py
@@ -2,12 +2,32 @@ import gym
 import numpy as np
 
 
+class Card:
+    def __init__(self, id, name, type, cost, attack, defense, keywords,
+                 player_hp, enemy_hp, card_draw):
+        self.id = id
+        self.name = name
+        self.type = type
+        self.cost = cost
+        self.attack = attack
+        self.defense = defense
+        self.keywords = keywords
+        self.player_hp = player_hp
+        self.enemy_hp = enemy_hp
+        self.card_draw = card_draw
+
+    def has_ability(self, keyword):
+        return keyword in self.keywords
+
+
 class LoCMEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
     def __init__(self, use_draft_history=True):
         self.state = None
         self.turn = 1
+
+        self._cards = self._load_cards()
 
         cards_in_state = 33 if use_draft_history else 3
         card_features = 16
@@ -23,10 +43,27 @@ class LoCMEnv(gym.Env):
         self.action_space = gym.spaces.Discrete(3)
 
     def reset(self):
-        pass
+        self.state = np.full(self.state_shape, 0, dtype=np.float32)
+        self.turn = 1
+
+        self.draft = self._new_draft()
 
     def step(self, action):
         pass
 
     def render(self, mode='human'):
         pass
+
+    @staticmethod
+    def _load_cards():
+        cards = []
+
+        with open('gym_locm/cardlist.txt', 'r') as card_list:
+            raw_cards = card_list.readlines()
+
+            for card in raw_cards:
+                cards.append(Card(*map(str.strip, card.split(';'))[:-1]))
+
+        assert len(cards) == 160
+
+        return cards
