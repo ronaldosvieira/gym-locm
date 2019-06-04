7c95bed1a91d8988a3803c3ced3e6cfefcb9683d
Ronaldo Vieira <ronaldoesv@gmail.com>
2019-06-04 16:11:06 -0300

    Implement env initialization

--- gym_locm/envs/locm_env.py
+++ gym_locm/envs/locm_env.py
@@ -1,11 +1,31 @@
 import gym
+import numpy as np
 
 
 class LoCMEnv(gym.Env):
-    def step(self, action):
-        pass
+    metadata = {'render.modes': ['human']}
+
+    def __init__(self, use_draft_history=True):
+        self.state = None
+        self.turn = 1
+
+        cards_in_state = 33 if use_draft_history else 3
+        card_features = 16
+
+        self.state_shape = (cards_in_state, card_features)
+
+        self.observation_space = gym.spaces.Box(
+            low=-1.0, high=1.0,
+            shape=self.state_shape,
+            dtype=np.float32
+        )
+
+        self.action_space = gym.spaces.Discrete(3)
 
     def reset(self):
+        pass
+
+    def step(self, action):
         pass
 
     def render(self, mode='human'):
--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@ from setuptools import setup
 
 setup(name='gym_locm',
       version='0.0.1',
-      install_requires=['gym'])
+      install_requires=['gym', 'numpy'])
