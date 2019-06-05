fa242b82bd8cf2296bd8c9f83956e574cb260842
Ronaldo Vieira <ronaldoesv@gmail.com>
2019-06-05 12:39:07 -0300

    Remove responsability for game related stuff from env

--- gym_locm/envs/locm_env.py
+++ gym_locm/envs/locm_env.py
@@ -24,17 +24,16 @@ class LoCMEnv(gym.Env):
     metadata = {'render.modes': ['human']}
     card_types = {'creature': 0, 'itemGreen': 1, 'itemRed': 2, 'itemBlue': 3}
 
-    def __init__(self, use_draft_history=True):
+    def __init__(self, use_draft_history=True, cards_in_deck=30):
         self.state = None
         self.turn = 1
 
-        self._cards = self._load_cards()
-        self._draft = None
+        # self._draft = None
 
         self.cards_in_state = 33 if use_draft_history else 3
         self.card_features = 16
 
-        self.cards_in_deck = 30
+        self.cards_in_deck = cards_in_deck
         self.state_shape = (self.cards_in_state, self.card_features)
 
         self.observation_space = gym.spaces.Box(
@@ -48,8 +47,7 @@ class LoCMEnv(gym.Env):
     def reset(self):
         self.turn = 1
 
-        self.draft = self._new_draft()
-        self.state = self.draft[self.turn - 1]
+        # self.state = self.draft[self.turn - 1]
 
         return self._convert_state()
 
@@ -58,14 +56,6 @@ class LoCMEnv(gym.Env):
 
     def render(self, mode='human'):
         print(self._convert_state())
-
-    def _new_draft(self):
-        draft = []
-
-        for _ in range(self.cards_in_deck):
-            draft.append(np.random.choice(self._cards, 3, replace=False).tolist())
-
-        return draft
 
     def _convert_state(self):
         converted_state = np.full((3, self.card_features), 0, dtype=np.float32)
@@ -89,23 +79,3 @@ class LoCMEnv(gym.Env):
             )
 
         return converted_state
-
-    @staticmethod
-    def _load_cards():
-        cards = []
-
-        with open('gym_locm/cardlist.txt', 'r') as card_list:
-            raw_cards = card_list.readlines()
-
-            for card in raw_cards:
-                id, name, card_type, cost, attack, defense, \
-                keywords, player_hp, enemy_hp, card_draw, _ = \
-                    map(str.strip, card.split(';'))
-
-                cards.append(Card(int(id), name, card_type, int(cost),
-                                  int(attack), int(defense), keywords,
-                                  int(player_hp), int(enemy_hp), int(card_draw)))
-
-        assert len(cards) == 160
-
-        return cards
