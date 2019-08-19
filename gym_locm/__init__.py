dcc8faeeab75be39e70d51951991b823ff068305
Ronaldo Vieira <ronaldo.vieira@dcc.ufmg.br>
2019-08-19 15:59:25 -0300

    Register single player draft env to gym

--- gym_locm/__init__.py
+++ gym_locm/__init__.py
@@ -2,3 +2,5 @@ from gym.envs.registration import register
 
 register(id='LoCM-draft-v0',
          entry_point='gym_locm.envs:LoCMDraftEnv')
+register(id='LoCM-draft-single-v0',
+         entry_point='gym_locm.envs:LoCMDraftSingleEnv')
