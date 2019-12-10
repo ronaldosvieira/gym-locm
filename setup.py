f82f97e9fcf8bd7c77164f6cdc6ce7f1a3d06c04
Ronaldo Vieira <ronaldo.vieira@dcc.ufmg.br>
2019-12-10 10:38:59 -0300

    Add missing dependency to setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='gym_locm',
       version='0.0.1',
-      install_requires=['gym', 'numpy', 'prettytable', 'pexpect'],
+      install_requires=['gym', 'numpy', 'prettytable', 'pexpect', 'sty'],
       extras_require={
             'pca': ['mplcursors', 'pandas', 'matplotlib', 'scikit-learn'],
             'experiment': ['numpy', 'scipy', 'stable_baselines']
