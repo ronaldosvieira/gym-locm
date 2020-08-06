835d732ed087de3bb4aff5ccf35d97996846f9ab
Ronaldo Vieira <ronaldo.vieira@dcc.ufmg.br>
2020-08-06 10:03:02 -0300

    Implement assymetric self-play

--- gym_locm/util/trainer2.py
+++ gym_locm/util/trainer2.py
@@ -3,6 +3,7 @@ import logging
 import os
 import time
 import warnings
+import numpy as np
 from abc import abstractmethod
 from datetime import datetime
 from statistics import mean
@@ -19,11 +20,11 @@ tf.get_logger().setLevel(logging.ERROR)
 
 from stable_baselines import PPO2
 from stable_baselines.common.policies import MlpPolicy, MlpLstmPolicy
-from stable_baselines.common.callbacks import CheckpointCallback, EvalCallback
 from stable_baselines.common.vec_env import VecEnv, DummyVecEnv
 
 from gym_locm.agents import Agent, MaxAttackDraftAgent, MaxAttackBattleAgent
 from gym_locm.envs import LOCMDraftSingleEnv
+from gym_locm.envs.draft import LOCMDraftSelfPlayEnv
 
 verbose = True
 
@@ -216,6 +217,186 @@ class FixedAdversary(TrainingSession):
             e.close()
 
 
+class AsymmetricSelfPlay(TrainingSession):
+    def __init__(self, env_builder, eval_env_builder, model_builder,
+                 train_episodes, eval_episodes, num_evals,
+                 num_switches, params, path, seed, num_envs=1):
+        super(AsymmetricSelfPlay, self).__init__(params, path, seed)
+
+        # log start time
+        start_time = time.perf_counter()
+
+        # initialize parallel training environments
+        self.logger.debug("Initializing training envs...")
+        env1, env2 = [], []
+
+        for i in range(num_envs):
+            # no overlap between episodes at each process
+            current_seed = seed + (train_episodes // num_envs) * i
+
+            # create one env per process
+            env1.append(env_builder(seed=current_seed, play_first=True))
+            env2.append(env_builder(seed=current_seed, play_first=False))
+
+        # wrap envs in a vectorized env
+        self.env1 = DummyVecEnv([lambda: e for e in env1])
+        self.env2 = DummyVecEnv([lambda: e for e in env2])
+
+        # initialize parallel evaluating environments
+        self.logger.debug("Initializing evaluation envs...")
+        self.evaluator: Evaluator = Evaluator(eval_env_builder, eval_episodes,
+                                              seed + train_episodes, num_envs)
+
+        # build the models
+        self.logger.debug("Building the models...")
+        self.model1 = model_builder(self.env1, seed, **params)
+        self.model1.adversary = model_builder(self.env2, seed, **params)
+        self.model2 = model_builder(self.env2, seed, **params)
+        self.model2.adversary = model_builder(self.env1, seed, **params)
+
+        # initialize parameters of adversary models accordingly
+        self.model1.adversary.load_parameters(self.model2.get_parameters(), exact_match=True)
+        self.model2.adversary.load_parameters(self.model1.get_parameters(), exact_match=True)
+
+        # set adversary models as adversary policies of the self-play envs
+        def make_adversary_policy(model, env):
+            def adversary_policy(obs):
+                zero_completed_obs = np.zeros((num_envs,) + env.observation_space.shape)
+                zero_completed_obs[0, :] = obs
+
+                actions, _ = model.adversary.predict(zero_completed_obs)
+
+                return actions[0]
+
+            return adversary_policy
+
+        self.env1.set_attr('adversary_policy',
+                           make_adversary_policy(self.model1, self.env1))
+        self.env2.set_attr('adversary_policy',
+                           make_adversary_policy(self.model2, self.env2))
+
+        # create necessary folders
+        os.makedirs(path + '/role0', exist_ok=True)
+        os.makedirs(path + '/role1', exist_ok=True)
+
+        # set tensorflow log dirs
+        self.model1.tensorflow_log = path + '/role0'
+        self.model2.tensorflow_log = path + '/role1'
+
+        # save parameters
+        self.train_episodes = train_episodes
+        self.eval_episodes = eval_episodes
+        self.num_evals = num_evals
+        self.num_switches = num_switches
+        self.eval_frequency = train_episodes / num_evals
+
+        # initialize control attributes
+        self.model1.last_eval, self.model1.next_eval = None, 0
+        self.model2.last_eval, self.model2.next_eval = None, 0
+        self.model1.role_id, self.model2.role_id = 0, 1
+
+        # initialize results
+        self.checkpoints = [], []
+        self.win_rates = [], []
+        self.episode_lengths = [], []
+        self.action_histograms = [], []
+
+        # log end time
+        end_time = time.perf_counter()
+
+        self.logger.debug("Finished initializing training session "
+                          f"({round(end_time - start_time, ndigits=3)}s).")
+
+    def _training_callback(self, _locals=None, _globals=None):
+        model = _locals['self']
+        episodes_so_far = model.num_timesteps // 30
+
+        # if it is time to evaluate, do so
+        if episodes_so_far >= model.next_eval:
+            # save model
+            model_path = self.path + f'/{episodes_so_far}'
+            model.save(model_path)
+            self.logger.debug(f"Saved model at {model_path}.zip.")
+
+            # evaluate the model
+            self.logger.info(f"Evaluating model ({episodes_so_far} episodes)...")
+            start_time = time.perf_counter()
+
+            mean_reward, ep_length, act_hist = \
+                self.evaluator.run(RLDraftAgent(model),
+                                   play_first=model.role_id == 0)
+
+            end_time = time.perf_counter()
+            self.logger.info(f"Finished evaluating "
+                             f"({round(end_time - start_time, 3)}s). "
+                             f"Avg. reward: {mean_reward}")
+
+            # save the results
+            self.checkpoints[model.role_id].append(episodes_so_far)
+            self.win_rates[model.role_id].append((mean_reward + 1) / 2)
+            self.episode_lengths[model.role_id].append(ep_length)
+            self.action_histograms[model.role_id].append(act_hist)
+
+            # update control attributes
+            model.last_eval = episodes_so_far
+            model.next_eval += self.eval_frequency
+
+        # if training should end, return False to end training
+        training_is_finished = episodes_so_far >= self.train_episodes
+
+        if training_is_finished:
+            self.logger.debug(f"Training ended at {episodes_so_far} episodes")
+
+        return not training_is_finished
+
+    def _train(self):
+        # save and evaluate starting models
+        self._training_callback({'self': self.model1})
+        self._training_callback({'self': self.model2})
+
+        try:
+            episodes_per_switch = self.train_episodes // self.num_switches
+            self.logger.debug(f"Training will switch models every "
+                              f"{episodes_per_switch} episodes")
+
+            for _ in range(self.num_switches):
+                # train the first player model
+                self.model1.learn(total_timesteps=30 * episodes_per_switch,
+                                  reset_num_timesteps=False,
+                                  callback=self._training_callback)
+                self.logger.debug(f"Model {self.model1.role_id} trained for "
+                                  f"{self.model1.num_timesteps // 30} episodes. "
+                                  f"Switching to model {self.model2.role_id}.")
+
+                # train the second player model
+                self.model2.learn(total_timesteps=30 * episodes_per_switch,
+                                  reset_num_timesteps=False,
+                                  callback=self._training_callback)
+                self.logger.debug(f"Model {self.model2.role_id} trained for "
+                                  f"{self.model2.num_timesteps // 30} episodes. "
+                                  f"Switching to model {self.model1.role_id}.")
+
+                # update parameters of adversary models
+                self.model1.adversary.load_parameters(self.model2.get_parameters(),
+                                                      exact_match=True)
+                self.model2.adversary.load_parameters(self.model1.get_parameters(),
+                                                      exact_match=True)
+                self.logger.debug("Parameters of adversary networks updated.")
+        except KeyboardInterrupt:
+            pass
+
+        # save and evaluate final models, if not done yet
+        if len(self.win_rates[0]) < self.num_evals:
+            self._training_callback({'self': self.model1})
+
+        if len(self.win_rates[1]) < self.num_evals:
+            self._training_callback({'self': self.model1})
+
+        # close the envs
+        for e in (self.env1, self.env2, self.evaluator):
+            e.close()
+
+
 class Evaluator:
     def __init__(self, env_builder, episodes, seed, num_envs):
         # log start time
@@ -320,13 +501,19 @@ class Evaluator:
 
 if __name__ == '__main__':
     def build_env(seed=None, play_first=True):
-        env = LOCMDraftSingleEnv(seed=seed, draft_agent=MaxAttackDraftAgent(),
-                                 battle_agents=(MaxAttackBattleAgent(), MaxAttackBattleAgent()),
-                                 use_draft_history=False, use_mana_curve=False,
-                                 play_first=play_first)
+        battle_agents = (MaxAttackBattleAgent(), MaxAttackBattleAgent())
 
-        return lambda: env
+        return LOCMDraftSelfPlayEnv(seed=seed, play_first=play_first,
+                                    battle_agents=battle_agents,
+                                    use_draft_history=False, use_mana_curve=False)
 
+    def build_eval_env(seed=None, play_first=True):
+        adversary_draft_agent = MaxAttackDraftAgent()
+        battle_agents = (MaxAttackBattleAgent(), MaxAttackBattleAgent())
+
+        return LOCMDraftSingleEnv(seed=seed, draft_agent=adversary_draft_agent,
+                                  battle_agents=battle_agents, play_first=play_first,
+                                  use_draft_history=False, use_mana_curve=False)
 
     def build_model(env, seed, neurons, layers, activation, n_steps, nminibatches,
                     noptepochs, cliprange, vf_coef, ent_coef, learning_rate):
@@ -338,15 +525,27 @@ if __name__ == '__main__':
                     n_steps=n_steps, nminibatches=nminibatches,
                     noptepochs=noptepochs, cliprange=cliprange,
                     vf_coef=vf_coef, ent_coef=ent_coef,
-                    learning_rate=learning_rate, tensorboard_log=None)
+                    learning_rate=learning_rate, tensorboard_log='models/trashcan/trash03/tf_log/',
+                    n_cpu_tf_sess=env.num_envs)
+        '''net_arch = ['lstm'] + [neurons] * layers
+        activation = dict(tanh=tf.nn.tanh, relu=tf.nn.relu, elu=tf.nn.elu)[activation]
+
+        return PPO2(MlpLstmPolicy, env, verbose=0, gamma=1, seed=seed,
+                    policy_kwargs=dict(net_arch=net_arch, n_lstm=neurons, act_fun=activation),
+                    n_steps=n_steps, nminibatches=nminibatches,
+                    noptepochs=noptepochs, cliprange=cliprange,
+                    vf_coef=vf_coef, ent_coef=ent_coef,
+                    learning_rate=learning_rate, tensorboard_log=None,
+                    n_cpu_tf_sess=env.num_envs)'''
 
 
-    params = {'layers': 1, 'neurons': 29, 'n_steps': 210, 'nminibatches': 30,
+    params = {'layers': 1, 'neurons': 29, 'n_steps': 30, 'nminibatches': 1,#30,
               'noptepochs': 19, 'cliprange': 0.1, 'vf_coef': 1.0,
               'ent_coef': 0.00781891437626065, 'learning_rate': 0.0001488768154153614,
               'activation': 'tanh'}
 
-    ts = TrainingSession(build_env, build_env, build_model, 3000, 300, 12, True, params,
-                        'models/trashcan/trash03', 36987, num_processes=2)
+    ts = AsymmetricSelfPlay(build_env, build_eval_env, build_model, 3000, 300,
+                            12, 10, params, 'models/trashcan/trash03', 36987,
+                            num_envs=4)
 
     ts.run()
