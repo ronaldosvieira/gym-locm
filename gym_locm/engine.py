ca7085781cf23b622129cce8544234044fe97bda
Ronaldo Vieira <ronaldo.vieira@dcc.ufmg.br>
2020-08-09 21:49:39 -0300

    Add state from native input as static method of State class

--- gym_locm/engine.py
+++ gym_locm/engine.py
@@ -1052,5 +1052,63 @@ class State:
 
         return new_copy
 
+    @staticmethod
+    def from_native_input(game_input):
+        state = State()
+
+        game_input = iter(game_input)
+
+        for i, player in enumerate(state.players):
+            health, mana, deck, rune, draw = map(int, next(game_input).split())
+
+            player.health = health
+            player.mana = mana
+            player.base_mana = mana
+            player.next_rune = rune
+            player.bonus_draw = 0 if i == 0 else draw - 1
+            player.last_drawn = draw if i == 0 else 1
+
+            player.hand = []
+            player.deck = [Card.mockup_card() for _ in range(deck)]
+
+        state.phase = Phase.DRAFT if mana == 0 else Phase.BATTLE
+
+        opp_hand, opp_actions = map(int, next(game_input).split())
+
+        state.opposing_player.hand = [Card.mockup_card() for _ in range(opp_hand)]
+
+        for _ in range(opp_actions):
+            next(game_input)
+
+        card_count = int(next(game_input))
+
+        for _ in range(card_count):
+            card_id, instance_id, location, card_type, \
+            cost, attack, defense, keywords, player_hp, \
+            opp_hp, card_draw, lane = next(game_input).split()
+
+            card_type = int(card_type)
+
+            types_dict = {0: Creature, 1: GreenItem, 2: RedItem, 3: BlueItem}
+
+            card_class = types_dict[card_type]
+
+            card = card_class(int(card_id), "", card_type, int(cost),
+                              int(attack), int(defense), keywords,
+                              int(player_hp), int(opp_hp), int(card_draw),
+                              "", instance_id=int(instance_id))
+
+            location = int(location)
+            lane = int(lane)
+
+            if location == 0:
+                state.players[0].hand.append(card)
+            elif location == 1:
+                state.players[0].lanes[lane].append(card)
+            elif location == -1:
+                state.players[1].lanes[lane].append(card)
+
+        return state
+
 
 Game = State
