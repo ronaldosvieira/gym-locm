dd4f6067621658faca6a2cb92334576f2f7a61dd
Ronaldo Vieira <ronaldoesv@gmail.com>
2020-09-29 12:43:06 -0300

    Group all optional dependencies under the 'experiments' label

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(name='gym_locm',
       version='0.0.1',
       install_requires=['gym', 'numpy', 'prettytable', 'pexpect', 'sty'],
       extras_require={
-            'pca': ['mplcursors', 'pandas', 'matplotlib', 'scikit-learn'],
-            'rl': ['numpy', 'scipy', 'stable_baselines', 'hyperopt'],
+            'experiments': ['numpy', 'scipy', 'stable_baselines', 'hyperopt',
+                            'mplcursors', 'pandas', 'matplotlib', 'scikit-learn'],
       },
       entry_points={
             'console_scripts': [
