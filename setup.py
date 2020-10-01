cb2aaf79128608f77907967a05996be305e74a65
Ronaldo Vieira <ronaldoesv@gmail.com>
2020-10-01 10:08:20 -0300

    Bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='gym_locm',
-      version='0.0.1',
+      version='1.0.0',
       install_requires=['gym', 'numpy', 'prettytable', 'pexpect', 'sty'],
       extras_require={
             'experiments': ['numpy', 'scipy', 'stable_baselines', 'hyperopt',
