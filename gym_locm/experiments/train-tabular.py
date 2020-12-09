7eae9b11091432cfbfe8ad96f40ece0b316c8d17
Ronaldo Vieira <ronaldoesv@gmail.com>
2020-12-09 09:38:17 -0300

    Enable process parallelism on tabular q-learning

--- gym_locm/experiments/train-tabular.py
+++ gym_locm/experiments/train-tabular.py
@@ -1,68 +1,91 @@
+import multiprocessing
 import numpy as np
 
 from gym_locm.agents import MaxAttackDraftAgent, MaxAttackBattleAgent
-from gym_locm.envs.draft import LOCMDraftSingleTabularEnv, LOCMDraftEnv
+from gym_locm.envs.draft import LOCMDraftSingleTabularEnv
 
 
-def _new_q_table(env: LOCMDraftEnv):
-    q = dict()
+def _init_q_table():
+    q_table = dict()
 
     for c1 in range(160):
         for c2 in range(c1 + 1, 160):
             for c3 in range(c2 + 1, 160):
-                for action in range(env.k):
-                    q[c1, c2, c3, action] = 0
+                for action in range(k):
+                    q_table[c1, c2, c3, action] = 0
 
-    q[None] = 0
+    q_table[None] = 0
 
-    return q
-
-
-def _get_policy_for_state(q: dict, state: tuple):
-    return np.argmax([q[(*state, 0)], q[(*state, 1)], q[(*state, 2)]])
+    return q_table
 
 
-def run():
+def _get_policy_for_state(state: tuple):
+    try:
+        return np.argmax([q[(*state, 0)], q[(*state, 1)], q[(*state, 2)]])
+    except TypeError:
+        return 0
+
+
+def _get_best_q_for_state(state: tuple):
+    try:
+        return np.max([q[(*state, 0)], q[(*state, 1)], q[(*state, 2)]])
+    except TypeError:
+        return 0
+
+
+def _iterate():
+    with iteration.get_lock():
+        iteration.value += 1
+
+        return iteration.value
+
+
+def q_learning(worker_id: int):
     env = LOCMDraftSingleTabularEnv(
         draft_agent=MaxAttackDraftAgent(),
         battle_agents=(MaxAttackBattleAgent(), MaxAttackBattleAgent()),
-        evaluation_battles=1
+        evaluation_battles=1, k=k, n=n
     )
 
-    n_episodes = 1_000_000
-    gamma = 1
-    alpha = lambda i: 0.5 / i if i > 0 else 0
-    epsilon = lambda i: 1.0 - (i / n_episodes)
+    state = env.reset()
 
-    q = _new_q_table(env)
+    print(f"Worker {worker_id} starting")
 
-    for iteration in range(n_episodes * 30):
-        if (iteration + 1) % 1000 == 0:
-            print(f"Iteration {iteration + 1}")
+    while True:
+        current_iteration = _iterate()
 
-        state = env.reset()
-        done = False
+        if current_iteration >= n_episodes * n:
+            break
 
-        while not done:
-            best_action = _get_policy_for_state(q, state)
+        if (current_iteration + 1) % 1000 == 0:
+            print(f"Iteration {current_iteration + 1}")
 
-            if np.random.random() <= epsilon(iteration):
-                action = env.action_space.sample()
-            else:
-                action = best_action
+        best_action = _get_policy_for_state(state)
 
-            new_state, reward, done, info = env.step(action)
+        if np.random.random() <= epsilon(current_iteration):
+            action = env.action_space.sample()
+        else:
+            action = best_action
 
-            if new_state is not None:
-                best_new_action = _get_policy_for_state(q, new_state)
-            else:
-                best_new_action = 0
+        new_state, reward, done, info = env.step(action)
 
-            q[(*state, action)] = q[(*state, action)] \
-                + alpha(iteration) \
-                * (reward + gamma * best_new_action - q[(*state, action)])
+        best_q_new_state = _get_best_q_for_state(new_state)
 
-            state = new_state
+        q[(*state, action)] += \
+            alpha(current_iteration) \
+            * (reward + gamma * best_q_new_state - q[(*state, action)])
+
+        state = new_state if not done else env.reset()
+
+
+def run():
+    processes = multiprocessing.Pool(processes=4)
+
+    for i in range(4):
+        processes.apply_async(q_learning, [i])
+
+    processes.close()
+    processes.join()
 
     with open('policy.csv', 'w+') as policy:
         policy.write("c1;c2;c3;pi\n")
@@ -70,11 +93,22 @@ def run():
         for c1 in range(160):
             for c2 in range(c1 + 1, 160):
                 for c3 in range(c2 + 1, 160):
-                    policy.write(f"{c1};{c2};{c3};{_get_policy_for_state(q, (c1, c2, c3))}")
-                    policy.write("\n")
+                    policy.write(f"{c1};{c2};{c3};{_get_policy_for_state((c1, c2, c3))}\n")
 
     print("✅")
 
 
 if __name__ == '__main__':
+    k = 3
+    n = 30
+
+    n_episodes = 30_000_000
+    gamma = 1
+    alpha = lambda i: 0.5 / i if i > 0 else 0
+    epsilon = lambda i: 1.0 - (i / (n_episodes * n))
+
+    manager = multiprocessing.Manager()
+    q = manager.dict(_init_q_table())
+    iteration = multiprocessing.Value('l', 0)
+
     run()
