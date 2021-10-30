4435352abe3d2a5b71f81c1b120cc0eb775f5460
Ronaldo e Silva Vieira <ronaldo.vieira@dcc.ufmg.br>
2021-10-30 14:56:32 -0300

    Improve battle training script

--- gym_locm/experiments/training-battle.py
+++ gym_locm/experiments/training-battle.py
@@ -1,16 +1,19 @@
 import gym
-from sb3_contrib import MaskablePPO
-from sb3_contrib.common.maskable.evaluation import evaluate_policy
-
 import gym_locm
+from sb3_contrib import MaskablePPO
+from stable_baselines3.common.callbacks import CheckpointCallback, EvalCallback
+from stable_baselines3.common.env_util import make_vec_env
 
-seed = 20211030
-env = gym.make('LOCM-battle-v0', seed=seed)
-model = MaskablePPO("MlpPolicy", env, gamma=1, seed=seed, verbose=1)
+path = 'trained_models/battle/ppo_mask'
+n_envs = 4
 
-model.learn(1_000_000)
+env = make_vec_env('LOCM-battle-v0', n_envs=n_envs)
+eval_env = make_vec_env('LOCM-battle-v0', n_envs=n_envs)
 
-evaluate_policy(model, env, n_eval_episodes=100, reward_threshold=0, warn=True)
+model = MaskablePPO("MlpPolicy", env, gamma=1, verbose=1)
 
-model.save('trained_models/battle/ppo_mask')
+checkpoint_callback = CheckpointCallback(save_freq=10_000 // n_envs, save_path=path + '/models')
 
+model.learn(1_000_000, callback=checkpoint_callback)
+
+model.save('trained_models/battle/ppo_mask/final')
