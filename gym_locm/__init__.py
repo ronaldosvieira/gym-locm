63d0aab19e970129638745e8b272ae5c329ef38b
Ronaldo e Silva Vieira <ronaldo.vieira@dcc.ufmg.br>
2022-01-18 10:09:13 -0300

    Register contructed envs for gym

--- gym_locm/__init__.py
+++ gym_locm/__init__.py
@@ -12,6 +12,12 @@ register(id='LOCM-draft-v0',
 register(id='LOCM-draft-2p-v0',
          entry_point='gym_locm.envs:LOCMDraftEnv')
 
+register(id='LOCM-constructed-v0',
+         entry_point='gym_locm.envs:LOCMConstructedSingleEnv')
+
+register(id='LOCM-constructed-2p-v0',
+         entry_point='gym_locm.envs:LOCMConstructedEnv')
+
 register(id='LOCM-battle-v0',
          entry_point='gym_locm.envs:LOCMBattleSingleEnv')
 
