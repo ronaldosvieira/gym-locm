351ee77a51c668e9f02c2b8cc9a0bb7d0d984f4b
Ronaldo Vieira <ronaldo.vieira@dcc.ufmg.br>
2022-04-26 12:46:53 -0300

    Implement win-loss reward function

--- gym_locm/envs/rewards.py
+++ gym_locm/envs/rewards.py
@@ -9,7 +9,19 @@ class RewardFunction(ABC):
         pass
 
 
-available_rewards = {}
+class WinLossRewardFunction(RewardFunction):
+    def calculate(self, state: State, for_player: PlayerOrder = PlayerOrder.FIRST):
+        if state.winner == for_player:
+            return 1
+        elif state.winner == for_player.opposing():
+            return -1
+        else:
+            return 0
+
+
+available_rewards = {
+    "win-loss": WinLossRewardFunction
+}
 
 
 def parse_reward(reward_name: str):
