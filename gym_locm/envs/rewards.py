29dc1f0e8498956c1c8efb1fc46a85bc151747d7
Ronaldo Vieira <ronaldo.vieira@dcc.ufmg.br>
2022-05-05 13:08:26 -0300

    Implement Coac state eval reward function

--- gym_locm/envs/rewards.py
+++ gym_locm/envs/rewards.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+from gym_locm.agents import CoacBattleAgent
 from gym_locm.engine import State, PlayerOrder
 
 
@@ -39,12 +40,20 @@ class OpponentBoardPresenceRewardFunction(RewardFunction):
         return -sum(creature.attack for lane in state.players[for_player.opposing()].lanes for creature in lane)
 
 
+class CoacRewardFunction(RewardFunction):
+    def calculate(self, state: State, for_player: PlayerOrder = PlayerOrder.FIRST):
+        signal = 1 if state.current_player.id == for_player else -1
+
+        return min(1, max(-1, signal * CoacBattleAgent.eval_state(state) / 2000))
+
+
 available_rewards = {
     "win-loss": WinLossRewardFunction,
     "player-health": PlayerHealthRewardFunction,
     "opponent-health": OpponentHealthRewardFunction,
     "player-board-presence": PlayerBoardPresenceRewardFunction,
-    "opponent-board-presence": OpponentBoardPresenceRewardFunction
+    "opponent-board-presence": OpponentBoardPresenceRewardFunction,
+    "coac": CoacRewardFunction
 }
 
 
