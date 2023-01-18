f82d1b0462425fda69bca5db2758a3a2bc72b15a
Ronaldo e Silva Vieira <ronaldo.vieira@dcc.ufmg.br>
2023-01-18 14:25:55 -0300

    Add support for random number generators

--- gym_locm/card_generator.py
+++ gym_locm/card_generator.py
@@ -2,17 +2,40 @@ import json
 import os
 from typing import Dict
 
+import numpy as np
 
-def load_card_weights() -> Dict:
+_card_weights = None
+_rng = None
+
+
+def _get_card_weights() -> Dict:
     """
     Read the LOCM 1.5 procedural card generator weights file.
     Available at
     https://github.com/acatai/Strategy-Card-Game-AI-Competition/blob/master/referee1.5-java/src/main/resources/cardWeights.json
     """
-    with open(os.path.dirname(__file__) + "/resources/cardWeights.json") as weights_file:
-        weights_json = json.load(weights_file)
+    global _card_weights
 
-    return weights_json
+    if _card_weights is None:
+        with open(
+            os.path.dirname(__file__) + "/resources/cardWeights.json"
+        ) as weights_file:
+            _card_weights = json.load(weights_file)
+
+    return _card_weights
 
 
-_card_weights = load_card_weights()
+def _get_rng() -> np.random.Generator:
+    global _rng
+
+    if _rng is None:
+        _rng = np.random.default_rng()
+
+    return _rng
+
+
+def generate_card(rng: np.random.Generator = None):
+    if rng is None:
+        rng = _get_rng()
+
+    card_weights = _get_card_weights()
