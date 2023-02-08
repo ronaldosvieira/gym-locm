d5a147bcb9afdabe88b55dbeece9b349e5f90267
Ronaldo e Silva Vieira <ronaldo.vieira@dcc.ufmg.br>
2023-02-08 10:57:14 -0300

    Implement consistency check for version 1.5

--- gym_locm/toolbox/consistency-checker.py
+++ gym_locm/toolbox/consistency-checker.py
@@ -13,11 +13,18 @@ def extract_match_json(match_line):
     return match_json
 
 
-def extract_match_transitions(match_json):
+def extract_match_transitions(match_json, version="1.5"):
     player1_states = list(filter(bool, match_json["errors"]["0"]))
     player2_states = list(filter(bool, match_json["errors"]["1"]))
     player1_actions = list(map(str.rstrip, filter(bool, match_json["outputs"]["0"])))
     player2_actions = list(map(str.rstrip, filter(bool, match_json["outputs"]["1"])))
+
+    if version == "1.5":
+        player1_states[0] += player1_states[1]
+        player1_states.remove(player1_states[1])
+
+        player2_states[0] += player2_states[1]
+        player2_states.remove(player2_states[1])
 
     return dict(
         player1=dict(states=player1_states, actions=player1_actions),
@@ -25,11 +32,16 @@ def extract_match_transitions(match_json):
     )
 
 
-def _find_deck_order(match_transitions, player):
+def _find_deck_order(match_transitions, player, version="1.5"):
     known_instance_ids = set()
     deck_order = []
 
-    for battle_state in match_transitions[player]["states"][30:]:
+    if version == "1.5":
+        start_index = 1
+    else:
+        start_index = 30
+
+    for battle_state in match_transitions[player]["states"][start_index:]:
         state = State.from_native_input(battle_state)
 
         for card in state.current_player.hand:
@@ -40,9 +52,9 @@ def _find_deck_order(match_transitions, player):
     return deck_order
 
 
-def find_deck_orders(match_transitions):
-    return _find_deck_order(match_transitions, "player1"), _find_deck_order(
-        match_transitions, "player2"
+def find_deck_orders(match_transitions, version="1.5"):
+    return _find_deck_order(match_transitions, "player1", version), _find_deck_order(
+        match_transitions, "player2", version
     )
 
 
@@ -57,11 +69,26 @@ def find_draft_options(match_transitions):
     return draft
 
 
-def recreate_initial_state(match_transitions, draft_options, deck_orders, phase=Phase.DECK_BUILDING):
+def find_constructed_options(match_transitions):
+    state = State.from_native_input(match_transitions["player1"]["states"][0])
+
+    return list(state.current_player.hand)
+
+
+def recreate_initial_state(
+    match_transitions,
+    deck_building_options,
+    deck_orders,
+    phase=Phase.DECK_BUILDING,
+    version="1.5",
+):
     if phase == Phase.DECK_BUILDING:
         start_index = 0
     else:
-        start_index = 30
+        if version == "1.5":
+            start_index = 1
+        else:
+            start_index = 30
 
     state = State.from_native_input(
         match_transitions["player1"]["states"][start_index],
@@ -69,19 +96,32 @@ def recreate_initial_state(match_transitions, draft_options, deck_orders, phase=
     )
 
     if phase == Phase.DECK_BUILDING:
-        state._phase._draft_cards = draft_options
-    else:
+        if version == "1.5":
+            state._phase._constructed_cards = deck_building_options
+        else:
+            state._phase._draft_cards = deck_building_options
+    elif phase == Phase.BATTLE:
         state.opposing_player.base_mana -= 1
         state.opposing_player.bonus_mana += 1
 
     return state
 
 
-def validate_match(match_transitions, state, p1_agent, p2_agent, phase=Phase.DECK_BUILDING):
+def validate_match(
+    match_transitions,
+    state,
+    p1_agent,
+    p2_agent,
+    phase=Phase.DECK_BUILDING,
+    version="1.5",
+):
     if phase == Phase.DECK_BUILDING:
         start_index = 0
     else:
-        start_index = 30
+        if version == "1.5":
+            start_index = 1
+        else:
+            start_index = 30
 
     p1_states_iter = iter(match_transitions["player1"]["states"][start_index:])
     p1_actions_iter = iter(match_transitions["player1"]["actions"][start_index:])
@@ -103,9 +143,7 @@ def validate_match(match_transitions, state, p1_agent, p2_agent, phase=Phase.DEC
 
         remove_trailing_pass_action = state.is_battle()
 
-        assert (
-            original_state == recreated_state
-        ), f"{original_state}\n{recreated_state}"
+        assert original_state == recreated_state, f"{original_state}\n{recreated_state}"
 
         print("States match!")
 
@@ -113,6 +151,10 @@ def validate_match(match_transitions, state, p1_agent, p2_agent, phase=Phase.DEC
             map(str.strip, next(actions_iter[player]).split(";"))
         )
         recreated_actions = []
+
+        # workaround: ByteRL always outputs a PASS action at the end
+        if len(original_actions) > 1 and original_actions[-1] == "PASS":
+            original_actions.pop()
 
         print("Original actions:", original_actions)
 
@@ -146,7 +188,7 @@ def validate_match(match_transitions, state, p1_agent, p2_agent, phase=Phase.DEC
         print("Actions match!")
 
 
-def run():
+def check_version_12():
     dataset_path = "gym_locm/engine/resources/consistency-dataset-v1.2.txt"
 
     with open(dataset_path, "r") as dataset:
@@ -164,7 +206,6 @@ def run():
     )
 
     for i, match in enumerate(matches):
-
         p1_agent.reset()
         p2_agent.reset()
 
@@ -174,7 +215,7 @@ def run():
 
         print(match_json)
 
-        transitions = extract_match_transitions(match_json)
+        transitions = extract_match_transitions(match_json, version="1.2")
 
         p1 = transitions["player1"]["states"]
         p2 = transitions["player2"]["states"]
@@ -186,22 +227,91 @@ def run():
         print(len(p1a), p1a[30:])
         print(len(p2a), p2a[30:])
 
-        deck_orders = find_deck_orders(transitions)
+        deck_orders = find_deck_orders(transitions, version="1.2")
         print(deck_orders)
 
         draft_options = find_draft_options(transitions)
 
-        state = recreate_initial_state(transitions, draft_options, deck_orders, Phase.DRAFT)
+        state = recreate_initial_state(
+            transitions, draft_options, deck_orders, Phase.DRAFT, version="1.2"
+        )
 
-        validate_match(transitions, state, p1_agent, p2_agent, Phase.DRAFT)
+        validate_match(transitions, state, p1_agent, p2_agent, Phase.DRAFT, version="1.2")
 
-        state = recreate_initial_state(transitions, draft_options, deck_orders, Phase.BATTLE)
+        state = recreate_initial_state(
+            transitions, draft_options, deck_orders, Phase.BATTLE, version="1.2"
+        )
 
-        validate_match(transitions, state, p1_agent, p2_agent, Phase.BATTLE)
+        validate_match(transitions, state, p1_agent, p2_agent, Phase.BATTLE, version="1.2")
+
+    p1_agent.close()
+    p2_agent.close()
+
+
+def check_version_15():
+    dataset_path = "gym_locm/engine/resources/consistency-dataset-v1.5.txt"
+
+    with open(dataset_path, "r") as dataset:
+        matches = dataset.readlines()
+
+    print(len(matches))
+
+    p1_agent = NativeAgent(
+        "/home/ronaldo/.virtualenvs/gym-locm/bin/python ../../IdeaProjects/Strategy-Card-Game-AI-Competition/contest-2022-08-COG/ByteRL/locm1d5_submit.py",
+        verbose=True,
+    )
+    p2_agent = NativeAgent(
+        "/home/ronaldo/.virtualenvs/gym-locm/bin/python ../../IdeaProjects/Strategy-Card-Game-AI-Competition/contest-2022-08-COG/ByteRL/locm1d5_submit.py",
+        verbose=True,
+    )
+
+    for i, match in enumerate(matches):
+        p1_agent.reset()
+        p2_agent.reset()
+
+        print(f"Match #{i}")
+
+        match_json = extract_match_json(match)
+
+        print(match_json)
+
+        transitions = extract_match_transitions(match_json, version="1.5")
+
+        p1 = transitions["player1"]["states"]
+        p2 = transitions["player2"]["states"]
+        p1a = transitions["player1"]["actions"]
+        p2a = transitions["player2"]["actions"]
+
+        print(len(p1), p1[1:])
+        print(len(p2), p2[1:])
+        print(len(p1a), p1a[1:])
+        print(len(p2a), p2a[1:])
+
+        deck_orders = find_deck_orders(transitions, version="1.5")
+        print(deck_orders)
+
+        constructed_options = find_constructed_options(transitions)
+
+        state = recreate_initial_state(
+            transitions,
+            constructed_options,
+            deck_orders,
+            Phase.CONSTRUCTED,
+            version="1.5",
+        )
+
+        validate_match(transitions, state, p1_agent, p2_agent, Phase.CONSTRUCTED, version="1.5")
+
+        state = recreate_initial_state(
+            transitions, constructed_options, deck_orders, Phase.BATTLE, version="1.5"
+        )
+
+        validate_match(transitions, state, p1_agent, p2_agent, Phase.BATTLE, version="1.5")
 
     p1_agent.close()
     p2_agent.close()
 
 
 if __name__ == "__main__":
-    run()
+    check_version_12()
+    check_version_15()
