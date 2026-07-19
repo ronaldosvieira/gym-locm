2438f9b7a2530ef988fa1ee08ff4019af75fe24b
Ronaldo Vieira <ronaldoesv@gmail.com>
2026-07-19 10:44:43 -0300

    Update hello_world_battle to use GUI and ByteRL agents

--- hello_world_battle.py
+++ hello_world_battle.py
@@ -4,19 +4,17 @@ from gym_locm import agents
 
 
 def hello_world():
+    p1 = agents.ByteRL()
+    p2 = agents.ByteRL()
+
     env = gym.make(
-        "LOCM-battle-v0",
-        deck_building_agents=(
-            agents.RandomConstructedAgent(),
-            agents.RandomConstructedAgent(),
-        ),
-        battle_agent=(agents.RandomBattleAgent()),
+        "LOCM-battle-2p-v0",
+        deck_building_agents=(p1, p2),
         reward_functions=["win-loss", "opponent-health"],
         reward_weights=[1.0, 1.0],
         seed=42,
+        render_mode="gui"
     )
-
-    agent = agents.GreedyBattleAgent()
 
     obs, info = env.reset()
     terminated = False
@@ -24,12 +22,17 @@ def hello_world():
 
     while not (terminated or truncated):
         env.render()
+        current_player = env.unwrapped.state.current_player.id
+        agent = p1 if current_player == 0 else p2
+        
         action = agent.act(env.unwrapped.state)
-        print("Action:", action)
+        print(f"Player {current_player} Action:", action)
 
         obs, reward, terminated, truncated, info = env.step(action)
 
         print("Reward:", reward, info["raw_rewards"])
+        
+    env.render()
 
 
 if __name__ == "__main__":
